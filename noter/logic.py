2ec96e34d98e6f6d550c54a42fb3a1ab145e3c95
Jake Pierson <awhooshingwind@gmail.com>
2022-08-16 00:30:46 -0700

    better base template, view template WIP, logic.py started

--- noter/logic.py
+++ noter/logic.py
@@ -1,1 +1,10 @@
- 
+import markdown
+import numpy as np
+import pandas as pd
+
+# Helper functions
+
+# Render markdown
+extensions = ['mdx_math', 'extra', 'codehilite']
+def make_md(some_text):
+    return markdown.markdown(some_text, extensions=extensions)
